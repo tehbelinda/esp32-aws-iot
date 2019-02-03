2b82b69d75675b9eaaef325b9a7dd3cc79beb2fa
Belinda Teh <belinda.teh@tri.global>
2019-02-03 15:54:27 -0800

    Clean up main.py

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ from umqtt.simple import MQTTClient
 # Update to your server and port
 SERVER = '<FILL ME IN>.iot.us-east-1.amazonaws.com'
 PORT = 8883
-TOPIC = 'friendstatus'
+TOPIC = '<FILL IN TOPIC>'
 # Update path to your credentials
 PRIVATE_KEY = 'security/<FILL ME IN>-private.pem.key'
 CERTIFICATE = 'security/<FILL ME IN>-certificate.pem.crt'
 # Update this to unique client id
-CLIENT_ID = 'friend0'
+CLIENT_ID = '<FILL IN CLIENT ID>'
 
 
 def setup_aws_iot():
@@ -35,7 +35,6 @@ def setup_aws_iot():
 
     def on_message(topic, msg):
         print('Topic: %s, msg: %s' % (topic, msg))
-        # Flash LED
     client.set_callback(on_message)
 
     client.connect()
@@ -50,21 +49,7 @@ def setup_aws_iot():
 
 
 if __name__ == '__main__':
-    # Test LEDs work
-    np_pin = machine.Pin(12, machine.Pin.OUT)
-    np = neopixel.NeoPixel(np_pin, 32, timing=True)
-    np[0] = (0, 50, 0)
-    np.write()
-
     client = setup_aws_iot()
 
-    for i in range(32):
-        np[i] = (0, 50, 50)
-        time.sleep(1)
-        np.write()
-
-    # Set up button pin -> run client.publish
-    # while True:
-    #     client.check_msg()
-    #     time.sleep(1)
+    # TODO: # Set up button pin -> run client.publish
  
